fddb9067cc5dd393d03a365ffc60bf1a84faa453
DanielHmmr <111769036+DanielHmmr@users.noreply.github.com>
2024-11-20 22:11:55 +0100

    Update wsgi.py

--- ns/wsgi.py
+++ ns/wsgi.py
@@ -3,6 +3,6 @@ import os
 
 if __name__ == "__main__":
     # Get the port from the environment variable
-    port = int(os.environ.get("PORT", 4000))  # Default to 4000 for local testing
+    port = int(os.environ.get("PORT", 10000))  # Default to 4000 for local testing
     print(f"Starting app on port {port}")  # Log the port to confirm
     app.run(host="0.0.0.0", port=port)
