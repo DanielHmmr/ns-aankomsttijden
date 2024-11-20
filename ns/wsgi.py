de4192f469361c8454f1dc5c0049e80504545ea0
DanielHmmr <111769036+DanielHmmr@users.noreply.github.com>
2024-11-20 21:57:08 +0100

    Update wsgi.py

--- ns/wsgi.py
+++ ns/wsgi.py
@@ -2,6 +2,6 @@ from app import app
 import os
 
 if __name__ == "__main__":
-    # Get the port from the environment variable (this will be set by Render)
-    port = int(os.environ.get("PORT", 4000))  # Default to 4000 if PORT is not set
+    # Fetch the port from the environment variable set by Render
+    port = int(os.environ.get("PORT", 4000))  # Use 4000 as fallback if PORT is not set
     app.run(host="0.0.0.0", port=port)
